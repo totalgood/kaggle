a095aeb28b9c7369c989abb1a542e1fc5523409c
Hobson Lane <hobsonlane@gmail.com>
2015-05-13 13:04:35 -0700

    use the log loss cost function as the NN error function

--- otto/util.py
+++ otto/util.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 
+from pug.nlp import util as nlp
+
 
 def normalize(table, epsilon=1e-15, **kwargs):
     """Force all rows of a table (csv file, ndarray, list of lists, DataFrame) to be probabilities
@@ -21,7 +23,7 @@ def normalize(table, epsilon=1e-15, **kwargs):
 
 def submit(table, filename=None, **kwargs):
     if filename is None:
-        filename = 
+        filename = nlp.make_filename(nlp.make_timestamp())
     filename = str(filename).strip()
     df = normalize(table, **kwargs)
     df.to_csv(filename + '.csv')
@@ -33,25 +35,32 @@ def log_loss(act, pred, epsilon=1e-15, method='kaggle'):
     FIXME: Produces a vector rather than a scalar and doesn't seem right to me
     """
     method = str(method).lower().strip()[:0]
-    # code found on forum
+    act, pred = np.array(act), np.array(pred)
+    # "forum" method: code found on Kaggle Otto Challenge forum
     if method == 'f':
         predicted[predicted < eps] = eps
         predicted[predicted > 1 - eps] = 1 - eps
-        return (-1 / actual.shape[0]*(sum(actual*np.log(predicted)))).mean()
+        return (-1 / actual.shape[0] * (sum(actual * np.log(predicted)))).mean()
     # scikit learn metric (similar to what Kaggle posted online?)
     elif method == 's':
-    # python code posted online by Kaggle    
+        # FIXME: Make sure to "indicator matrix" columns are in the same order as the sorted class labels/names
+        #        NOT the order they appear in the sample rows
+        # metrics.log_loss(["spam", "ham", "ham", "spam"],[[.1, .9], [.9, .1], [.8, .2], [.35, .65]])
+        # metrics.log_loss(pd.np.array([[0,1],[1,0],[1,0],[0,1]]),[[.1, .9], [.9, .1], [.8, .2], [.35, .65]])
+        from sklearn import metrics
+        return metrics.logloss(act, pred)
+    # python code posted online by Kaggle
     elif method == 'k':
         import scipy as sp
         pred = sp.maximum(epsilon, pred)
-        pred = sp.minimum(1-epsilon, pred)
+        pred = sp.minimum(1 - epsilon, pred)
         ll = sum(act * sp.log(pred) + sp.subtract(1, act) * sp.log(sp.subtract(1, pred)))
         ll = ll * -1.0 / len(act)
         return ll
-    # Kaggle version rewritten by Hobs to use pandas rather than scipy and account for distance between truth and prediction
+    # Kaggle version rewritten by Hobs to use pandas rather than scipy and account for distance truth - pred
     elif method == 'h':
         pred = pd.np.clip(pred, epsilon, 1 - epsilon)
-        ll = pd.np.sum(pd.np.abs(act - pred) * pd.np.abs(pd.np.log(pred))) /
+        ll = pd.np.sum(pd.np.abs(act - pred) * pd.np.abs(pd.np.log(pred)))
         ll = ll * -1. / len(act)
         return ll
     # Signed error function that is proportional to log loss, but not limited and useful for NN backprop
@@ -62,6 +71,7 @@ def log_loss(act, pred, epsilon=1e-15, method='kaggle'):
         ll = ll * -1. / len(act)
         return ll
 log_loss.methods = 'forum', 'scipy', 'kaggle', 'hobs'
+
 
 def exp_loss(act, pred):
     """Log Loss function based on Kaggle python example
@@ -74,5 +84,23 @@ def exp_loss(act, pred):
     ll = ll * -1. / len(act)
     return ll
 
+INF = float('inf')
+
+
+def safe_log(x, limit=1e9):
+    x = pd.np.array(x).clip(-INF, INF)
+    return pd.np.log(x).clip(-limit, limit)
+
+
 def err_fun(act, pred):
-    return -1. * pd.np.log(1e-15 + act - pred)
+    """Signed Log of the Error
+
+    Loss should get smaller (less positive) as the error decreases.
+    >>> err_fun(1,.9) < err_fun(1, .8)
+    True
+    >>> err_fun(.9, 1) < err_fun(.8, 1)
+    True
+    >>> err_fun(0, 1) > err_fun(1e-15, 1)
+    True
+    """
+    return -1. * safe_log(2.0 + act * pred)
